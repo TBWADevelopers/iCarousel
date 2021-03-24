75354a5a1e445b4eb09d53d4b119d09e6786f817
Vadim Pavlov <vadim.pavlov@tbwamobile.com>
2021-03-24 17:16:37 +0200

    removed dependencies

--- Package.swift
+++ Package.swift
@@ -20,8 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "iCarousel",
-            path: "iCarousel",
-            dependencies: []),
+            path: "iCarousel"),
         .testTarget(
             name: "iCarouselTests",
             dependencies: ["iCarousel"]),
